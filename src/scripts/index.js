e6667b2c3591f0e2a60be9858b196797882f4f61
Thomas Hendry <thomas.hendry@hva.nl>
2026-04-11 21:36:47 +0200

    add template

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,11 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('click', (e) => {
   switch (e.target.dataset.do) {
-    case 'open-blank':
-      openBlank(e);
-      return;
     case 'add-picture':
       addPicture(e);
+      return;
+    case 'add-blank':
+      addBlank(e);
+      return;
+    case 'open-blank':
+      openBlank(e);
       return;
     case 'save-drawing':
       saveCanvas(e);
@@ -24,6 +27,22 @@ document.addEventListener('click', (e) => {
 async function addPicture(e) {
   console.log('api test')
   await actions.apiTest();
+}
+
+function addBlank(e) {
+  const title = 'Blank';
+  
+  const templatePicsItemElems = document.getElementById('template-pics-item').content.children;
+  if (templatePicsItemElems === null) return;
+
+  const picsElem = document.getElementById('blank');
+  if (picsElem === null) return;
+
+  const picsItemElem = templatePicsItemElems[0].cloneNode(true);
+  const insertTitle = picsItemElem.querySelector('[data-insert="new-title"]');
+  insertTitle.textContent = title;
+  
+  picsElem.append(picsItemElem);
 }
 
 function openBlank(e) {
