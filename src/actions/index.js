890cd5b90ce6a7aff30a78e933158dc593e273e2
Thomas Hendry <thomas.hendry@hva.nl>
2026-04-15 16:08:27 +0200

    add more apis

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,28 @@ import { defineAction } from 'astro:actions';
 export const server = {
   getDuckUrl: defineAction({
     handler: async () => {
-      const url = 'https://random-d.uk/api/v2/random';
+      const url = 'https://random-d.uk/api/v2/random?type=jpg';
       const response = await fetch(url);
       const result = await response.json();
       const imageUrl = result.url;
+      return imageUrl;
+    }
+  }),
+  getCatUrl: defineAction({
+    handler: async () => {
+      const url = 'https://cataas.com/cat?json=true';
+      const response = await fetch(url);
+      const result = await response.json();
+      const imageUrl = result.url;
+      return imageUrl;
+    }
+  }),
+  getDogUrl: defineAction({
+    handler: async () => {
+      const url = 'https://dog.ceo/api/breeds/image/random';
+      const response = await fetch(url);
+      const result = await response.json();
+      const imageUrl = result.message;
       return imageUrl;
     }
   })
